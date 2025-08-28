a32af1928448c7ef4deffc00a5bf966166374b31
Ivan LG <boogue66@gmail.com>
2025-08-28 11:54:57 -0600

    Solucion de error de conexión a MongoDB.

--- src/database.js
+++ src/database.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 
 (async () => {
   try {
-    const db = await mongoose.connect(process.env.MONGODB_URI);
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Db connected to", db.connection.name);
   } catch (error) {
     console.error(error);
